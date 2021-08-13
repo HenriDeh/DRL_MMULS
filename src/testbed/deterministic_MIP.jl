44d3a7a034d9c50495e2f8925727667aca87c642
DrWatson <no@mail>
2021-08-13 10:45:24 +0200

    add comments to load_MIP

--- src/testbed/deterministic_MIP.jl
+++ src/testbed/deterministic_MIP.jl
@@ -4,40 +4,41 @@ using .Gurobi
 df = CSV.read("data/instances.csv", DataFrame)
 
 function load_deterministic_MIP(ins::DataFrameRow; forecasts, init_inv = :auto, prevQ = :auto)
-    EP = ins.EndProducts
-    RM = ins.RawMaterials
-    R = ins.Requirements |> Meta.parse |> eval |> first
-    CV = ins.CV
-    LT = ins.leadtimes |> Meta.parse |> eval |> first
-    h = ins.holdings |> Meta.parse |> eval |> first
-    b = ins.stockouts |> Meta.parse |> eval |> first
-    K = ins.setups |> Meta.parse |> eval |> first
-    μ = ins.mu |> Meta.parse |> eval |> first
-    I = size(R)[1]
-    α = ins.ressources |> Meta.parse |> eval |> first
-    C = ins.capacities |> Meta.parse |> eval |> first
-    Nres = length(C)
-    H = size(forecasts, 2)
-    @assert size(forecasts,1) == (EP) "forecasts must have $EP rows"
-    T = 9
-    H += T
-    d = [zeros(EP, T) forecasts]
+    EP = ins.EndProducts #number of end products
+    RM = ins.RawMaterials #number of raw materials
+    R = ins.Requirements |> Meta.parse |> eval |> first #matrix of BOM
+    CV = ins.CV #CV of demand
+    LT = ins.leadtimes |> Meta.parse |> eval |> first #LT of each item
+    h = ins.holdings |> Meta.parse |> eval |> first #holding cost of each item
+    b = ins.stockouts |> Meta.parse |> eval |> first #stockout cost of each item
+    K = ins.setups |> Meta.parse |> eval |> first #setup cost of each item
+    μ = ins.mu |> Meta.parse |> eval |> first #average per period demand of each item
+    I = size(R)[1] #number of items
+    α = ins.ressources |> Meta.parse |> eval |> first #matrix of capactity constraints' factors 
+    C = ins.capacities |> Meta.parse |> eval |> first #capactity of each ressource
+    Nres = length(C) #number of capactity constraints
+    H = size(forecasts, 2) #forecast horizon
+    @assert size(forecasts,1) == (EP) "forecasts must have $EP rows" #
+    T = 9 #startup time of the system: longest possible time needed to make an endproduct from scratch 
+    H += T 
+    d = [zeros(EP, T) forecasts] #demand is 0 during startup 
     
-    M = zeros(I, H)
-    M[1:EP,:] .= reverse(cumsum(reverse(d, dims=2), dims=2), dims=2)
+    M = zeros(I, H) #bigM of each product at each period
+    M[1:EP,:] .= reverse(cumsum(reverse(d, dims=2), dims=2), dims=2) #end products use cumulative demand
     for i in EP + 1:I
-        M[i,:] = [sum(R[i,j] * M[j,t] for j in 1:I) for t in 1:H]
+        M[i,:] = [sum(R[i,j] * M[j,t] for j in 1:I) for t in 1:H] #other items use internal cumulative demand
     end
     
-    s0 = init_inv == :auto ? init_inv = fill(0,I) : init_inv
+    s0 = init_inv == :auto ? init_inv = fill(0,I) : init_inv #initial inventories are 0 if not provided
 
     x0 = prevQ == :auto ? OffsetArray(fill(0.0, I, maximum(LT)), 1:I, 1-maximum(LT):0) : OffsetArray(prevQ, 1:I, 1-maximum(LT):0)
+    #on-order quantities. 0 if not provided.
 
     # compute SS
-    σ = CV .* forecasts
-    ratio = (b ./ (b .+ h))[1:EP]
-    SS = quantile.(Normal(), ratio) .* σ
-    SS = [zeros(EP, T) SS]
+    σ = CV .* forecasts #demand standard deviation
+    ratio = (b ./ (b .+ h))[1:EP] #critical ratio of end products
+    SS = quantile.(Normal(), ratio) .* σ #compute SS based on critical ratio
+    SS = [zeros(EP, T) SS] #SS is 0 during startup time.
 
     model = Model()
     @variable(model, s[i=1:I, t=0:H] >= 0)
