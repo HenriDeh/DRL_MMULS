baeb485ce7e57e19d06b9aecb035bbbb9794d6e1
DrWatson <no@mail>
2022-04-11 17:20:31 +0200

    setup for revised testset

--- scripts/single-item/forecast_generation.jl
+++ scripts/single-item/forecast_generation.jl
@@ -1,55 +1,44 @@
 #script used to generate forecasts.csv
 using Distributions, CSV, DataFrames
-CSV.write("data/single-item/forecasts.csv", DataFrame(ID = [],trend = [], deviation = [], forecast = []))
-T = 52
+CSV.write("data/single-item/forecasts.csv", DataFrame(ID = [],trend = [], forecast = []))
+T = 104
 H = 52
-μ = 10
-n = 50
-ϱs = [2, 4]
+μs = (5,10,15)
 ID = 0
 """Constant trends"""
 
-for ϱ in ϱs
-    δ = Uniform(-ϱ, ϱ)
-    for i in 1:n
-        global ID += 1
-        fc = [μ + rand(δ) for i in 1:T]
-        CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Constant", varrho = ϱ, forecast = [fc]), append = true)
-    end
+for μ in μs
+    global ID += 1
+    fc = [μ for i in 1:T]
+    CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Constant", forecast = [fc]), append = true)
 end
 
 """Seasonal trends"""
 
-fs = [1, 2]
-for ϱ in ϱs
-    δ = Uniform(-ϱ, ϱ)
-    for f in fs
-        for i in 1:n
-            global ID += 1
-            fc = [(1 + 0.5*sin(2f*t*π/H))*μ + rand(δ) for t in 1:T]
-            CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Seasonnal$f", varrho = ϱ, forecast = [fc]), append = true)
-        end
+fs = [0.5, 1, 2]
+
+for f in fs
+    for μ in μs
+        global ID += 1
+        fc = [(1 + 0.5*sin(2f*t*π/T))*μ for t in 1:T]
+        CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Seasonnal$f", forecast = [fc]), append = true)
     end
 end
+
 
 """Growth trends"""
 
-for ϱ in ϱs
-    δ = Uniform(-ϱ, ϱ)
-    for i in 1:n
-        global ID += 1
-        fc = [(0.8+0.4*t/T)*μ + rand(δ) for t in 1:T]
-        CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Growth", varrho = ϱ, forecast = [fc]), append = true)
-    end
+for μ in μs
+    global ID += 1
+    fc = [(0.8+0.4*t/T)*μ for t in 1:T]
+    CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Growth", forecast = [fc]), append = true)
 end
+
 
 """Decline trends"""
 
-for ϱ in ϱs
-    δ = Uniform(-ϱ, ϱ)
-    for i in 1:n
-        global ID += 1
-        fc = [(1.2-0.4*t/T)*μ + rand(δ) for t in 1:T]
-        CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Decline", varrho = ϱ, forecast = [fc]), append = true)
-    end
+for μ in μs
+    global ID += 1
+    fc = [(1.2-0.4*t/T)*μ for t in 1:T]
+    CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Decline", forecast = [fc]), append = true)
 end
