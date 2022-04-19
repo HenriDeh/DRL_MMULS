cbc9544aced49a6444f0a74d892a91da01a48c1e
DrWatson <no@mail>
2022-04-19 13:50:54 +0200

    forecasts

--- scripts/single-item/forecast_generation.jl
+++ scripts/single-item/forecast_generation.jl
@@ -3,42 +3,36 @@ using Distributions, CSV, DataFrames
 CSV.write("data/single-item/forecasts.csv", DataFrame(ID = [],trend = [], forecast = []))
 T = 104
 H = 52
-μs = (5,10,15)
+μ = 10
 ID = 0
-"""Constant trends"""
+"""Constant trend"""
 
-for μ in μs
-    global ID += 1
-    fc = [μ for i in 1:T]
-    CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Constant", forecast = [fc]), append = true)
-end
+global ID += 1
+fc = [μ for i in 1:T]
+CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Constant", forecast = [fc]), append = true)
+
 
 """Seasonal trends"""
 
 fs = [0.5, 1, 2]
 
 for f in fs
-    for μ in μs
-        global ID += 1
-        fc = [(1 + 0.5*sin(2f*t*π/T))*μ for t in 1:T]
-        CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Seasonnal$f", forecast = [fc]), append = true)
-    end
+    global ID += 1
+    fc = [(1 + 0.5*sin(2f*t*π/T))*μ for t in 1:T]
+    CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Seasonnal$f", forecast = [fc]), append = true)
 end
 
 
 """Growth trends"""
 
-for μ in μs
-    global ID += 1
-    fc = [(0.8+0.4*t/T)*μ for t in 1:T]
-    CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Growth", forecast = [fc]), append = true)
-end
+global ID += 1
+fc = [(0.8+0.4*t/T)*μ for t in 1:T]
+CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Growth", forecast = [fc]), append = true)
+
 
 
 """Decline trends"""
 
-for μ in μs
-    global ID += 1
-    fc = [(1.2-0.4*t/T)*μ for t in 1:T]
-    CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Decline", forecast = [fc]), append = true)
-end
+global ID += 1
+fc = [(1.2-0.4*t/T)*μ for t in 1:T]
+CSV.write("data/single-item/forecasts.csv", DataFrame(ID = ID, trend = "Decline", forecast = [fc]), append = true)
